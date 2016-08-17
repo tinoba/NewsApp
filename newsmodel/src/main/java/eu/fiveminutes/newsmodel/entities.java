b92c3ea3bd34d8464de237152393f7871e6e0b3a
tinoba <tinoba@net.hr>
2016-08-17 19:48:33 +0200

    podjela na module

--- newsmodel/src/main/java/eu/fiveminutes/newsmodel/entities.java
+++ newsmodel/src/main/java/eu/fiveminutes/newsmodel/entities.java
@@ -4,11 +4,14 @@ package eu.fiveminutes.newsmodel;
  * Created by tinoba on 17.8.2016..
  */
 final class Entities {
-    public final String lead_paragraph;
-    public final String snippet;
+    public final String Paragraph;
+    public final String PublishDate;
+    public final String HeadlineMainText;
 
-    public Entities(String lead_paragraph, String snippet){
-        this.lead_paragraph= lead_paragraph;
-        this.snippet = snippet;
+
+    public Entities(String Paragraph, String HeadlineText,String PublishDate){
+        this.Paragraph= Paragraph;
+        this.HeadlineMainText = HeadlineText;
+        this.PublishDate = PublishDate;
     }
 }
