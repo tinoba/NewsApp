c0cb0f633f77d0e3f3c4f709ece1c703fc237c05
tinoba <tinoba@net.hr>
2016-08-17 14:28:53 +0200

    butterknife onclick

--- app/src/main/java/eu/fiveminutes/news_app_2/MainActivity.java
+++ app/src/main/java/eu/fiveminutes/news_app_2/MainActivity.java
@@ -2,12 +2,23 @@ package eu.fiveminutes.news_app_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
+    @BindView(R.id.txt1)
+    TextView txt1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+    @OnClick(R.id.btnClickMe)
+    public void clickMe(){
+        txt1.setText("Clicked");
     }
 }
