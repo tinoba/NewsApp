5534069f41a9edbc07857e755585f74efc9921f1
tinoba <tinoba@net.hr>
2016-08-29 11:00:36 +0200

    Adding decelerate interpolator

--- app/src/main/java/eu/fiveminutes/newsapp/ui/fragment/NewsDetailFragment.java
+++ app/src/main/java/eu/fiveminutes/newsapp/ui/fragment/NewsDetailFragment.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
