215c3a8bb37fd3b71cd522b75cd854e24695f5da
tinoba <tinoba@net.hr>
2016-08-29 13:26:10 +0200

    Adding cache storing for webview

--- app/src/main/java/eu/fiveminutes/newsapp/ui/fragment/NewsDetailFragment.java
+++ app/src/main/java/eu/fiveminutes/newsapp/ui/fragment/NewsDetailFragment.java
@@ -5,7 +5,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Parcel;
 import android.os.Parcelable;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -14,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
@@ -21,21 +24,31 @@ import android.widget.ProgressBar;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import eu.fiveminutes.news_app_2.R;
+import eu.fiveminutes.newsapp.application.NewsApp;
+import eu.fiveminutes.newsapp.application.ObjectGraph;
 import eu.fiveminutes.newsapp.model.NewsArticle;
+import eu.fiveminutes.newsapp.ui.presenter.NewsDetailPresenter;
+import eu.fiveminutes.newsapp.ui.presenter.NewsDetailView;
 
-public final class NewsDetailFragment extends Fragment {
+public final class NewsDetailFragment extends Fragment implements NewsDetailView {
 
     private static final String NEWS_DETAIL = "NEWS_DETAIL";
 
     private NewsArticle article;
     private NewsArticleParcelable articleParcelable;
     private TitleListener titleListener;
+    private NewsDetailPresenter presenter;
 
     @BindView(R.id.news_detail_fragment_web_view)
     protected WebView newsDetailWebView;
 
     @BindView(R.id.news_detail_loading_bar)
     protected ProgressBar newsDetailLoadingBar;
+
+    private void setWebView() {
+        newsDetailWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        newsDetailWebView.loadUrl(article.webUrl);
+    }
 
     public static NewsDetailFragment newInstance(final NewsArticle article) {
         final NewsDetailFragment newsDetailFragment = new NewsDetailFragment();
@@ -47,12 +60,23 @@ public final class NewsDetailFragment extends Fragment {
         return newsDetailFragment;
     }
 
+    private void setWebViewSettings() {
+    }
+
+    @Override
+    public void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        final ObjectGraph objectGraph = ((NewsApp) getActivity().getApplication()).getObjectGraph();
+        presenter = objectGraph.provideNewsDetailPresenter();
+    }
+
     @Override
     public final View onCreateView(final LayoutInflater inflater, final ViewGroup container, final Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.fragment_news_detail, container, false);
         ButterKnife.bind(this, view);
 
         setHasOptionsMenu(true);
+        setWebViewSettings();
         final Bundle arguments = this.getArguments();
         if (arguments != null) {
             articleParcelable = arguments.getParcelable(NEWS_DETAIL);
@@ -66,7 +90,8 @@ public final class NewsDetailFragment extends Fragment {
     public void onResume() {
         super.onResume();
 
-        newsDetailWebView.loadUrl(article.webUrl);
+        presenter.setView(this);
+        setWebView();
         titleListener.setTitle(article.mainHeadline);
         newsDetailWebView.setWebViewClient(new WebViewClient() {
 
